53fb510fd64b9dc09f2d931920fda2e3dbd9412e
pinakigupta <pinaki.gupta@havalus.com>
2019-08-15 14:10:28 -0400

    somewhat working. Very slow to train.

--- Continuous_Control (copy).py
+++ Continuous_Control (copy).py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
+# To add a new cell, type '#%%'
+# To add a new markdown cell, type '#%% [markdown]'
+#%%
+#from IPython import get_ipython
 
+#%% [markdown]
 # # Continuous Control
 # 
 # ---
@@ -11,13 +14,15 @@
 # 
 # Run the next code cell to install a few packages.  This line will take a few minutes to run!
 
+#%%
+#get_ipython().system('pip -q install ./python')
+
+#%% [markdown]
 # The environments corresponding to both versions of the environment are already saved in the Workspace and can be accessed at the file paths provided below.  
 # 
 # Please select one of the two options below for loading the environment.
 
-# In[1]:
-
-
+#%%
 from unityagents import UnityEnvironment
 import numpy as np
 
@@ -27,26 +32,22 @@ env = UnityEnvironment(file_name='Reacher_Linux/Reacher.x86_64',no_graphics=True
 # select this option to load version 2 (with 20 agents) of the environment
 # env = UnityEnvironment(file_name='/data/Reacher_Linux_NoVis/Reacher.x86_64')
 
-
+#%% [markdown]
 # Environments contain **_brains_** which are responsible for deciding the actions of their associated agents. Here we check for the first brain available, and set it as the default brain we will be controlling from Python.
 
-# In[2]:
-
-
+#%%
 # get the default brain
 brain_name = env.brain_names[0]
 brain = env.brains[brain_name]
 for brains in env.brain_names:
     print(brains)
 
-
+#%% [markdown]
 # ### 2. Examine the State and Action Spaces
 # 
 # Run the code cell below to print some information about the environment.
 
-# In[3]:
-
-
+#%%
 # reset the environment
 env_info = env.reset(train_mode=True)[brain_name]
 
@@ -64,40 +65,35 @@ state_size = states.shape[1]
 print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
 print('The state for the first agent looks like:', states[0])
 
-
+#%% [markdown]
 # ### 3. Loading DDPG Agent
 # 
 
-# In[11]:
-
-
+#%%
 from importlib import reload 
 from collections import deque
 import ddpg_agent
 reload(ddpg_agent)
 import torch
+
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("DEVICE is ", DEVICE)
+#from workspace_utils import active_session
 
 
-# In[12]:
+#%%
+def play(env = None):
+    for i in range(3):
+        env_info = env.reset(train_mode=False)[brain_name] # reset the environment
+        state = env_info.vector_observations[0]            # get the current state
+        for j in range(200):
+            action = agent.act(state)
+            env.render()
+            state, reward, done, _ = env.step(action)
+            if done:
+                break 
 
-
-import matplotlib.pyplot as plt
-
-def plot_scores(scores):
-    # plot the scores
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    plt.plot(np.arange(len(scores)), scores)
-    plt.ylabel('Score')
-    plt.xlabel('Episode #')
-    plt.show()
-
-
-# In[13]:
-
-
+#%%
 def train(env = None, n_episodes=200, agent = None, 
          checkpoint_score = 25, breakpoint_score = 30, filename_prefix = ""):
     
@@ -153,31 +149,45 @@ def train(env = None, n_episodes=200, agent = None,
     return scores
 
 
-# In[14]:
 
+
+#%%
 
 agent = ddpg_agent.Agent(state_size=state_size, 
                          action_size=action_size, 
                          random_seed=0)
 
 
-# In[ ]:
-
-
+#%%
 rr_scores = train( env = env,
                    agent = agent) # random replay training
 
 
-# In[ ]:
+#%%
+import matplotlib.pyplot as plt
+
+def plot_scores(scores):
+    # plot the scores
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(np.arange(len(scores)), scores)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    plt.show()
 
 
+#%%
 plot_scores(rr_scores) # random replay scores
 
-
+#%% 
 # When finished, you can close the environment.
+# load the weights from file
+play(env = env)
 
-# In[6]:
 
+            
 
+#%%
 env.close()
+
 
--- ddpg_agent.py
+++ ddpg_agent.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import copy
+import os
 from collections import namedtuple, deque
 from importlib import reload 
 #from model import Actor, Critic
@@ -19,12 +20,13 @@ LR_ACTOR = 1e-4         # learning rate of the actor
 LR_CRITIC = 1e-4        # learning rate of the critic
 WEIGHT_DECAY = 0   	# L2 weight decay
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("DEVICE is ", DEVICE)
 
 class Agent():
     """Interacts with and learns from the environment."""
     
-    def __init__(self, state_size, action_size, random_seed, actor_arch, critic_arch):
+    def __init__(self, state_size, action_size, random_seed):
         """Initialize an Agent object.
         
         Params
@@ -38,24 +40,27 @@ class Agent():
         self.seed = random.seed(random_seed)
 
         # Actor Network (w/ Target Network)
-        self.actor_local = model.Actor(state_size, action_size, 
-                                       random_seed, actor_arch).to(device)
-        self.actor_target = model.Actor(state_size, action_size, 
-                                        random_seed, actor_arch).to(device)
+        self.actor_local = model.Actor(state_size, action_size, random_seed).to(DEVICE)
+        self.actor_target = model.Actor(state_size, action_size, random_seed).to(DEVICE)
         self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR)
 
         # Critic Network (w/ Target Network)
-        self.critic_local = model.Critic(state_size, action_size, 
-                                         random_seed, critic_arch).to(device)
-        self.critic_target = model.Critic(state_size, action_size, 
-                                          random_seed, critic_arch).to(device)
+        self.critic_local = model.Critic(state_size, action_size, random_seed).to(DEVICE)
+        self.critic_target = model.Critic(state_size, action_size, random_seed).to(DEVICE)
         self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
+
+        '''if os.path.isfile(self.CHECKPOINT_FOLDER + 'checkpoint_actor.pth') and os.path.isfile(self.CHECKPOINT_FOLDER + 'checkpoint_critic.pth'):
+            self.actor_local.load_state_dict(torch.load(self.CHECKPOINT_FOLDER + 'checkpoint_actor.pth'))
+            self.actor_target.load_state_dict(torch.load(self.CHECKPOINT_FOLDER + 'checkpoint_actor.pth'))
+
+            self.critic_local.load_state_dict(torch.load(self.CHECKPOINT_FOLDER + 'checkpoint_critic.pth'))
+            self.critic_target.load_state_dict(torch.load(self.CHECKPOINT_FOLDER + 'checkpoint_critic.pth'))'''
 
         # Noise process
         self.noise = OUNoise(action_size, random_seed)
 
         # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+        self.memory = ReplayBuffer(DEVICE, action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
     
     def step(self, state, action, reward, next_state, done):
         """Save experience in replay memory, and use random sample from buffer to learn."""
@@ -69,7 +74,7 @@ class Agent():
 
     def act(self, state, add_noise=True):
         """Returns actions for given state as per current policy."""
-        state = torch.from_numpy(state).float().to(device)
+        state = torch.from_numpy(state).float().to(DEVICE)
         self.actor_local.eval()
         with torch.no_grad():
             action = self.actor_local(state).cpu().data.numpy()
@@ -135,11 +140,16 @@ class Agent():
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
 
+    def checkpoint(self):
+        torch.save(self.actor_local.state_dict(), self.CHECKPOINT_FOLDER + 'checkpoint_actor.pth')      
+        torch.save(self.critic_local.state_dict(), self.CHECKPOINT_FOLDER + 'checkpoint_critic.pth')  
+
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
 
     def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
         """Initialize parameters and noise process."""
+        self.size = size        
         self.mu = mu * np.ones(size)
         self.theta = theta
         self.sigma = sigma
@@ -152,21 +162,23 @@ class OUNoise:
 
     def sample(self):
         """Update internal state and return it as a noise sample."""
-        x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        x = self.state        
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.standard_normal(self.size)
         self.state = x + dx
         return self.state
 
 class ReplayBuffer:
     """Fixed-size buffer to store experience tuples."""
 
-    def __init__(self, action_size, buffer_size, batch_size, seed):
+    def __init__(self, device, action_size, buffer_size, batch_size, seed):
         """Initialize a ReplayBuffer object.
         Params
         ======
             buffer_size (int): maximum size of buffer
             batch_size (int): size of each training batch
         """
+        self.DEVICE = device
+
         self.action_size = action_size
         self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
         self.batch_size = batch_size
@@ -182,11 +194,11 @@ class ReplayBuffer:
         """Randomly sample a batch of experiences from memory."""
         experiences = random.sample(self.memory, k=self.batch_size)
 
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(self.DEVICE)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(self.DEVICE)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(self.DEVICE)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(self.DEVICE)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(self.DEVICE)
 
         return (states, actions, rewards, next_states, dones)
 
